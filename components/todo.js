018dc1386f919ee70e98f89567205630c4d60953
Jin Wang <wangjin@wangjin.me>
2019-07-12 23:27:38 -0500

    JA-35: New API to return Todo list with Done Todos

--- components/todo.js
+++ components/todo.js
@@ -6,86 +6,91 @@ const sequelizeInstance = require("../models").database;
 
 const {todoCategory: TodoCategory, todo: Todo, comment: Comment} = require("../models");
 
+let _fetchTodoList = (done, res) => {
+    let categoryList = [];
+    sequelizeInstance.transaction(t =>
+        TodoCategory.findAll({
+            attributes: ["*", [Sequelize.fn("COUNT", Sequelize.col("todo.TodoId")), "todoCount"]],
+            where: {
+                todoCategoryStatus: true
+            },
+            include: [{
+                model: Todo,
+                attributes: [],
+                where: done ? 1 : {
+                    todoStatus: false
+                },
+                required: false
+            }],
+            group: ["todoCategory.todoCategoryId"],
+            transaction: t,
+            raw: true
+        })
+            .then(categoryData => {
+                categoryList = categoryData;
+                return Todo.findAll({
+                    attributes: ["*", [Sequelize.fn("COUNT", Sequelize.col("comment.commentId")), "commentCount"]],
+                    where: {
+                        [Sequelize.Op.or]: [
+                            {todoStatus: false},
+                            done ? {todoStatus: true} : 1,
+                            Sequelize.literal("DATE(todoUpdatedDate) = CURDATE()"),
+                            Sequelize.literal("DATE(todoCreatedDate) = CURDATE()")
+                        ]
+                    },
+                    include: [{
+                        model: Comment,
+                        attributes: [],
+                        where: {
+                            commentType: 1,
+                            commentDeleted: 0
+                        },
+                        required: false
+                    }],
+                    group: ["todo.todoId"],
+                    raw: true
+                });
+            })
+            .then(todoData => {
+                // Loop through the list to separate root todo items from sub todo items
+                let rootTodos = [];
+                let subTodos = [];
+                for (let i = 0; i < todoData.length; i++) {
+                    const todoItem = todoData[i];
+                    if (todoItem.parentTodoId !== null) {
+                        subTodos.push(todoItem);
+                    } else {
+                        todoItem.subTodos = [];
+                        rootTodos.push(todoItem);
+                    }
+                }
+                // Loop through the sub todos to put them into root todos
+                for (let j = 0; j < subTodos.length; j++) {
+                    const subTodoItem = subTodos[j];
+                    for (let k = 0; k < rootTodos.length; k++) {
+                        const rootTodoItem = rootTodos[k];
+                        if (subTodoItem.parentTodoId === rootTodoItem.todoId) {
+                            rootTodoItem.subTodos.push(subTodoItem);
+                        }
+                    }
+                }
+                // Insert todo items into its category list
+                for (let c = 0; c < categoryList.length; c++) {
+                    categoryList[c].todoList = [];
+                    for (let r = 0; r < rootTodos.length; r++) {
+                        if (categoryList[c].todoCategoryId === rootTodos[r].todoCategoryId) {
+                            categoryList[c].todoList.push(rootTodos[r]);
+                        }
+                    }
+                }
+                return output.apiOutput(res, {todoCategoryList: categoryList});
+            })
+    );
+};
+
 module.exports = {
     getTodoList (req, res) {
-        let categoryList = [];
-        sequelizeInstance.transaction(t =>
-            TodoCategory.findAll({
-                attributes: ["*", [Sequelize.fn("COUNT", Sequelize.col("todo.TodoId")), "todoCount"]],
-                where: {
-                    todoCategoryStatus: true
-                },
-                include: [{
-                    model: Todo,
-                    attributes: [],
-                    where: {
-                        todoStatus: false
-                    },
-                    required: false
-                }],
-                group: ["todoCategory.todoCategoryId"],
-                transaction: t,
-                raw: true
-            })
-                .then(categoryData => {
-                    categoryList = categoryData;
-                    return Todo.findAll({
-                        attributes: ["*", [Sequelize.fn("COUNT", Sequelize.col("comment.commentId")), "commentCount"]],
-                        where: {
-                            [Sequelize.Op.or]: [
-                                {todoStatus: false},
-                                Sequelize.literal("DATE(todoUpdatedDate) = CURDATE()"),
-                                Sequelize.literal("DATE(todoCreatedDate) = CURDATE()")
-                            ]
-                        },
-                        include: [{
-                            model: Comment,
-                            attributes: [],
-                            where: {
-                                commentType: 1,
-                                commentDeleted: 0
-                            },
-                            required: false
-                        }],
-                        group: ["todo.todoId"],
-                        raw: true
-                    });
-                })
-                .then(todoData => {
-                    // Loop through the list to separate root todo items from sub todo items
-                    let rootTodos = [];
-                    let subTodos = [];
-                    for (let i = 0; i < todoData.length; i++) {
-                        const todoItem = todoData[i];
-                        if (todoItem.parentTodoId !== null) {
-                            subTodos.push(todoItem);
-                        } else {
-                            todoItem.subTodos = [];
-                            rootTodos.push(todoItem);
-                        }
-                    }
-                    // Loop through the sub todos to put them into root todos
-                    for (let j = 0; j < subTodos.length; j++) {
-                        const subTodoItem = subTodos[j];
-                        for (let k = 0; k < rootTodos.length; k++) {
-                            const rootTodoItem = rootTodos[k];
-                            if (subTodoItem.parentTodoId === rootTodoItem.todoId) {
-                                rootTodoItem.subTodos.push(subTodoItem);
-                            }
-                        }
-                    }
-                    // Insert todo items into its category list
-                    for (let c = 0; c < categoryList.length; c++) {
-                        categoryList[c].todoList = [];
-                        for (let r = 0; r < rootTodos.length; r++) {
-                            if (categoryList[c].todoCategoryId === rootTodos[r].todoCategoryId) {
-                                categoryList[c].todoList.push(rootTodos[r]);
-                            }
-                        }
-                    }
-                    return output.apiOutput(res, {todoCategoryList: categoryList});
-                })
-        );
+        return _fetchTodoList(false, res);
     },
     createTodoItem (req, res) {
         // Validation
@@ -148,5 +153,8 @@ module.exports = {
         } else {
             output.error(res, "Please provide the Todo Item ID.");
         }
+    },
+    getDoneTodoList (req, res) {
+        return _fetchTodoList(true, res);
     }
 };
